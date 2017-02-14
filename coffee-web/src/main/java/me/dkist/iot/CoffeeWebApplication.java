eeb211ee9a2d42e4ebd838d5992671133ba626db
daniel <daniel@bridges.decora>
2017-02-14 13:36:11 -0200

    Initial structure and Decora status page

---
+++ coffee-web/src/main/java/me/dkist/iot/CoffeeWebApplication.java
@@ -0,0 +1,12 @@
+package me.dkist.iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeWebApplication.class, args);
+	}
+}
